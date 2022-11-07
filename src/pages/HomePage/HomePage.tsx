205072ff314303b9b6f465b785b580ec8dedffe9
Juan Camacho <juan@Juans-MacBook-Air.local>
2022-11-07 15:41:12 -0500

    dev: add router component

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from "react";
+
+const HomePage: FC<any> = () => {
+    return (
+        <>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+            Voluptate nobis, possimus quibusdam mollitia nesciunt tenetur 
+            cupiditate officia doloribus modi quod nisi ea, iusto 
+            quidem similique suscipit culpa commodi provident et?
+        </>
+    );
+}
+
+export default HomePage;
